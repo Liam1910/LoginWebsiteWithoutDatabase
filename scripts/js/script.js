166c4e100fca64205ac72e50ba41d41e915c8196
Liam1910 <108172258+Liam1910@users.noreply.github.com>
2022-10-06 08:18:49 +0000

    Add files via upload

---
+++ scripts/js/script.js
@@ -0,0 +1,47 @@
+function YouTube() {
+ document.getElementById("YouTubeP")
+  .innerHTML =
+  "https://www.youtube.com";
+}
+
+function FnF() {
+ document.getElementById("FnFP")
+  .innerHTML =
+  "https://www.kbhgames.com/friday-night-funkin";
+}
+
+function WinWorld() {
+ document.getElementById(
+   "WinWorldP")
+  .innerHTML =
+  "https://www.winworldpc.com";
+}
+
+function Webador() {
+ document.getElementById("WebadorP")
+  .innerHTML =
+  "https://www.webador.de";
+}
+
+function Pdf() {
+ document.getElementById("PdfP")
+  .innerHTML = "https://www.pdf.io"
+}
+
+function EasyEDA() {
+ document.getElementById("EasyEDAP")
+  .innerHTML =
+  "https://www.easyeda.com"
+}
+
+function Malware() {
+ document.getElementById("MalwareP")
+  .innerHTML =
+  "https://www.malwarewatch.org"
+}
+
+function DLMalware() {
+ document.getElementById(
+   "DLMalwareP").innerHTML =
+  "https://dl.malwarewatch.org"
+}
